859a54816888c1ee5ffd54b5d29686566d175a37
Neville Bonavia <neville.bonavia@betssongroup.com>
2017-02-12 15:03:08 +0100

    test finished

---
+++ js/components/gallery.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Image from './image';
+
+export default function Gallery(props) {
+    const images=props.images.map((image,index) => {
+        <Image url={image.url}
+                description={image.description}
+                key={index}/>
+    });
+
+    return (
+        <div className="gallery">
+            {images}
+        </div>
+    );
+}
